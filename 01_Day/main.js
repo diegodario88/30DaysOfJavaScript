55ff377bde5a2aec6f09197230db9098a43bcfc0
Diego Dario <diegodario88@gmail.com>
2020-01-06 00:41:08 -0300

    excercise 8 day-4

--- 01_Day/main.js
+++ 01_Day/main.js
@@ -1,3 +1,22 @@
-console.log(firstName, lastName, country, city, age, isMarried) 
+console.log(firstName, lastName, country, city, age, isMarried)
 console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
 console.log(name, job, live)
+
+
+
+
+const month = window.prompt('Enter month:').trim().toLowerCase()
+const monthFormated = month.charAt(0).toUpperCase() + month.substring(1)
+
+//#8 Day 4
+function daysOfMonth(month) {
+    switch (month) {
+        case 'January':
+            return alert(`${month} has ${new Date(2020, 0, 0).getDate()} days.`);
+
+        default:
+            break;
+    }
+
+}
+
